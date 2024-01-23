3b1eff30e505b1f7e0d6299136a701a9a29af069
AbujafarCh <357jafar@gmail.com>
2024-01-23 23:25:04 +0600

    professional hockey club edited

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        backgroundImages:{
+           'gridone': "url('/img/2.png')",
+        }
+      },
     },
     plugins: [],
   }
